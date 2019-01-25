e66769fbd322143f852a1dd49b890c2f1783bff2
Lovlesh Malik <lmalik@lovleshs-mbp.corp.adobe.com>
2019-01-25 15:35:26 +0530

    Adding Maven project

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void SimpleTest1() throws Exception {
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void SimpleTest2() throws Exception {
+		
+		assertTrue(false);
+	}
+}
